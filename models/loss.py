2bd1b5d66c2a746d28580878bdbc1170d856519b
Nicola Branchini <nicolabranchini@c104021.wlan.net.ed.ac.uk>
2020-02-03 17:52:09 +0000

    create three loss components in loss.py

--- models/loss.py
+++ models/loss.py
@@ -1,10 +1,36 @@
 
+import torch 
+from torch.nn import functional as F
 from torch.nn.modules.loss import _Loss
 
-class VaeGanLoss(_Loss):
-    def __init__(self, size_average=None, reduce=None, reduction='mean'):
-        super(VaeGanLoss, self).__init__(size_average, reduce, reduction)
+# class VaeGanLoss(_Loss):
+#     def __init__(self, size_average=None, reduce=None, reduction='mean'):
+#         super(VaeGanLoss, self).__init__(size_average, reduce, reduction)
 
-    def forward(self, input, target):
-        #TODO implement the loss
-        raise NotImplementedError
+#     def forward(self, input, target):
+#         #TODO implement the loss
+#         raise NotImplementedError
+
+
+def loss_llikelihood(discriminator, recon_x, x):
+
+	return F.mse_loss( discriminator.forward_l(recon_x) , discriminator.forward_l(x) , reduction= 'sum')
+
+
+def loss_prior(recon_x, x, mu, logvar):
+
+	return -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+
+def loss_gan(discriminator, decoder, z, x):
+
+	left = torch.log(discriminator.forward(x))
+
+	right = torch.log( 1. - discriminator(decoder.forward(z)) )
+
+	return left + right 
+
+
+def vae_gan_loss(discriminator, decoder, z, recon_x, x, mu, logvar):
+
+	return loss_prior(recon_x, x, mu, logvar) + loss_llikelihood(discriminator, recon_x, x) + loss_gan(discriminator, decoder, z, x)
--- models/vae.py
+++ models/vae.py
@@ -1,34 +1,35 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-from torchvision.utils import save_image
-import torch.utils.data
 from torch.nn import functional as F
-
-from comet_ml import Experiment
 
 import os
 
-class Discriminator(nn.Module):
+
+class VAE(nn.Module):
     def __init__(self):
-		super(Discriminator, self).__init__()
+        super(VAE, self).__init__()
 
-		layers = []
-		
-		layers.append(nn.Linear(in_features=28*28, out_features=512, bias=True))
-		layers.append(nn.LeakyReLU(0.2, inplace=True))
-		
-		layers.append(nn.Linear(in_features=512, out_features=256, bias=True))
-		layers.append(nn.LeakyReLU(0.2, inplace=True))
-		
-		layers.append(nn.Linear(in_features=256, out_features=1, bias=True))
-		layers.append(nn.Sigmoid())
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 784)
 
-		self.model = nn.Sequential(*layers)
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
 
-	def forward(self, x):
-		x = x.view(x.size(0), -1)
-		validity = self.model(x)
-		return validity
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
