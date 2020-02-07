bf09274ff257ff224c402b027b80f759f60c0780
Jevgenij Chom <jevgenijchom@gmail.com>
2020-02-07 16:34:41 +0000

    added dataloader

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -1,36 +1,28 @@
-""" 
-dataset class 
+import torch
+import torch.utils.data
+import torchvision.datasets
+import torchvision.transforms as transforms
 
-torch.utils.data.Dataset is an abstract class representing a dataset
-
-we inherit dataset and override __len__ and __getitem__
-
-"""
-
-from __future__ import print_function, division
-import os
-import torch 
-import pandas as pd
-from skimage import io, transform 
-import numpy as np 
-import matplotlib.pyplot as plt 
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils 
-
-# ignore warnings
-import warnings
-warnings.filterwarnings("ignore")
-
-# plt.ion() # interactive
-
-class CustomDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
+class DataLoader:
+    def __init__(self, root_dir, batch_size=128, shuffle=True, image_size=64, num_workers=2):
         """
         args:
-
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied on a sample.
         """
-        
-    def __len__(self):
-        return len(self. ) 
-    
-    def __getitem__(self, idx):
+        self.root_dir = root_dir
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.image_size = image_size
+        self.num_workers = num_workers
+
+    def get_dataloader(self):
+        dataset = torchvision.datasets.ImageFolder(root=self.root_dir,
+                                                   transform=transforms.Compose([
+                                                       transforms.Resize(self.image_size),
+                                                       transforms.ToTensor(),
+                                                       transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                                   ]))
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size,
+                                                 shuffle=self.shuffle, num_workers=self.num_workers)
+        return dataloader
